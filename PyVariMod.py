6b5bb0e9563998516e30b918efc732f957b9bcbb
Gavin Singleton <144152889+ccallisto@users.noreply.github.com>
2023-11-07 12:57:58 -0600

    connected to backend

--- PyVariMod.py
+++ PyVariMod.py
@@ -1,5 +1,6 @@
 # python test script for passing variables
-import sys
+import sys, boto3, json
+from botocore.config import Config
 
 def variableapasser():
     if len(sys.argv) == 4: # Checking if 3 arguments are passed (sys.argv[0] is the script name itself)
@@ -8,11 +9,31 @@ def variableapasser():
         drag = float(sys.argv[2])
         diameter = float(sys.argv[3])
         # test values for variable passing check{
-        print(f"Mass: {mass}")
-        print(f"Drag: {drag}")
-        print(f"Diameter: {diameter}")
+        # print(f"Mass: {mass}")
+        # print(f"Drag: {drag}")
+        # print(f"Diameter: {diameter}")
         # }
+        return mass, drag, diameter
     else:
         print("Expected 3 arguments: mass, drag, and diameter.")
-        return mass, drag, diameter
+        return 0
 variableapasser()
+
+mass, drag, diameter = variableapasser()
+
+lambda_client = boto3.client('lambda')
+test_event = {
+    "diameter": diameter,
+    "cd": drag,
+    "mass": mass
+}
+response = lambda_client.invoke(
+  FunctionName='simpleMathModel',
+  Payload=json.dumps(test_event),
+)
+#print(response['Payload'])
+
+apogee = float(response['Payload'].read().decode("utf-8"))
+#print(response['Payload'].read().decode("utf-8"))
+
+print("Altitude: %i ft" % apogee)
